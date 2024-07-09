4571d13c839607d415f033cd5056dde7c96b430c
Will Baxter <bax.will@googlemail.com>
2024-07-09 13:21:44 +0100

    Users can deleet own posts

--- src/middleware/posts.js
+++ src/middleware/posts.js
@@ -1,15 +1,30 @@
-const prisma = require('../utils/prisma')
+const prisma = require("../utils/prisma");
 
 async function verifyAdmin(req, res, next) {
-    const user = await prisma.user.findUnique({
-        where: {id: req.id}
-    })
-   
-    if (user.role !== 'ADMIN') {
-        return res.status(403).json( {error: 'Access denied - must be admin to access'} )
-    }
+  const user = await prisma.user.findUnique({
+    where: { id: req.id },
+    include: {
+      posts: true,
+    },
+  });
 
-    next()
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  if (post.userId === user.id) {
+    return next();
+  }
+
+  if (user.role !== "ADMIN") {
+    return res
+      .status(403)
+      .json({ error: "Access denied - must be admin to access" });
+  }
+
+  next();
 }
 
-module.exports = { verifyAdmin }
+module.exports = { verifyAdmin };
