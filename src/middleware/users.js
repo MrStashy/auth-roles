a8c433c7ac4eed792411efa50c227acf71f92ddd
Will Baxter <bax.will@googlemail.com>
2024-07-09 12:07:33 +0100

    Users can delete themselves

--- src/middleware/users.js
+++ src/middleware/users.js
@@ -17,6 +17,10 @@ async function verifyAdmin(req, res, next) {
     const user = await prisma.user.findUnique({
         where: {id: req.id}
     })
+
+    if (req.method === 'DELETE' && user.id === Number(req.params.id)) {
+        return next()
+    }
    
     if (user.role !== 'ADMIN') {
         return res.status(403).json( {error: 'Access denied - must be admin to access'} )
